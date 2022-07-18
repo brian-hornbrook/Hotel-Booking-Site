f6d33f0345df9d87719638df591171b1d2535337
brian-hornbrook <brian_hornbrook@outlook.com>
2022-07-18 14:28:49 -0500

    Updates

---
+++ precache-manifest.555a8e4ae9e94cb3dbfb163221a70cc6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ac7615b604326723fd7",
+    "url": "/Hotel-Booking-Site/static/css/main.847304bb.chunk.css"
+  },
+  {
+    "revision": "2ac7615b604326723fd7",
+    "url": "/Hotel-Booking-Site/static/js/main.b8770550.chunk.js"
+  },
+  {
+    "revision": "4e06e2d1ce8fb8c157a0",
+    "url": "/Hotel-Booking-Site/static/js/runtime~main.3702991c.js"
+  },
+  {
+    "revision": "a68ee97f505489edb6f3",
+    "url": "/Hotel-Booking-Site/static/js/2.f65867d9.chunk.js"
+  },
+  {
+    "revision": "b1de11d319e509137d84e1c8dae270b6",
+    "url": "/Hotel-Booking-Site/index.html"
+  }
+];
